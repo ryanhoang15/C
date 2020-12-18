ff68dc5730351a5aad63814254a1e54b10e58a09
ryanhoang15 <61032642+ryanhoang15@users.noreply.github.com>
2020-12-18 02:13:50 -0800

    Add files via upload

---
+++ megaWar/CardPile.cpp
@@ -0,0 +1,39 @@
+//
+//  CardPile.cpp
+//  Prog 6 - MegaWar
+//
+//  Created by ryan hoang on 12/1/20.
+//  Copyright © 2020 ryan hoang. All rights reserved.
+//
+
+//#include "CardPile.hpp"
+//#include "Card.hpp"
+//#include <iostream>
+//#include <vector>
+//using namespace std;
+//
+//void CardPile :: addCard(Card card){
+//    pile.push_back(card);
+//}
+//
+//Card CardPile :: removeCard(){
+//    Card temp = pile[pile.size() - 1];
+//    pile.pop_back();
+//    return temp;
+//}
+//
+//void CardPile :: display(){
+//    for(int i = 0; i < pile.size(); i++){
+//        if(i != 0 && i % 13 == 0){
+//            cout << endl;
+//        }
+//        pile[i].display();
+//    }
+//}
+//
+//
+//int CardPile :: pileSize(){
+//    return (int)pile.size();
+//}
+
+
