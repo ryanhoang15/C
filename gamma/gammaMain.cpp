9bbd8f76da1230ac76684fc3d6fc1f9d3c4489f3
ryanhoang15 <61032642+ryanhoang15@users.noreply.github.com>
2021-05-02 04:47:47 -0700

    Add files via upload

---
+++ gamma/gammaMain.cpp
@@ -0,0 +1,39 @@
+//
+//  main.cpp
+//  lab 9 Q#2
+//
+//  Created by ryan hoang on 10/28/20.
+//  Copyright © 2020 ryan hoang. All rights reserved.
+//
+
+/**
+@author Ryan Hoang
+@version Fall 2020
+@date 11/3/2020
+*/
+
+
+#include <iostream>
+#include "gamma.hpp"
+using namespace std;
+
+int main() {
+    
+    class gamma g1;
+
+    gamma::showtotal();
+
+    class gamma g2, g3;
+
+    gamma::showtotal();
+
+    
+    g1.showid();
+
+    g2.showid();
+
+    g3.showid();
+
+    cout << "----------end of program----------\n";
+    
+}
