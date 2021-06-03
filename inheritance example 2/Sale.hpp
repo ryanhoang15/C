8e741c66a326dddd1afc910f97b7352dbb2a98de
ryanhoang15 <61032642+ryanhoang15@users.noreply.github.com>
2021-06-03 09:04:46 -0700

    Add files via upload

---
+++ inheritance example 2/Sale.hpp
@@ -0,0 +1,25 @@
+//
+//  Sales.hpp
+//  lab 14
+//
+//  Created by ryan hoang on 11/30/20.
+//  Copyright © 2020 ryan hoang. All rights reserved.
+//
+
+#ifndef Sales_hpp
+#define Sales_hpp
+
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+class Sale{
+    
+public:
+    void getData();
+    void putData();
+    float saleArray[3];
+    
+};
+
+#endif /* Sales_hpp */
