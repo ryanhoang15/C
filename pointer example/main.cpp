20680610f9d8816e0af960748380264b7a320112
ryanhoang15 <61032642+ryanhoang15@users.noreply.github.com>
2021-06-07 00:09:51 -0700

    Add files via upload

--- pointer example/main.cpp
+++ pointer example/main.cpp
@@ -1,6 +1,6 @@
 //
 //  main.cpp
-//  Lab 6 Q#1
+//  Lab 6 Q#2
 //
 //  Created by ryan hoang on 10/5/20.
 //  Copyright © 2020 ryan hoang. All rights reserved.
@@ -17,20 +17,74 @@
 using namespace std;
 
 /**
- This main function's purpose is too create two long variables and a long pointer that points to value1 with a & in front
- display the address of vlaue1, address of longPtr, and value pointed by longPtr
+ *this function is printing the values of the array 
+ *@param *arr is the array that stores the value and is a pointer
+ *@param size is the size of the array
+ */
+void display(int * arr, int size){
+    for(int i = 0; i < size; i++){
+        cout << *arr << " ";
+        arr++;
+    }
+}
+
+
+/**
+ *this function is finding the max number in the array
+ *@param *arr is the array that stores the value and is a pointer
+ *@param size is the size of the array
+ *@return max is the biggest number in the array
+ */
+int max(int * arr, int size){
+    int max = *arr;
+    for(int i = 1; i < size; i++){
+        if(max < *(arr + i)){
+            max = *(arr + i);
+        }
+    }
+    return max;
+}
+
+
+
+/**this function is asking the user to enter the size of the array and then to enter its value
+ and after that the value is getting put into the array and than is getting sent to the display function and
+ the max function will return the max value
  */
 int main() {
-    long value1 = 200000;
-    long value2;
+    /**
+     These are the variables created
+     */
+    int listSize;
+    int value = 0;
     
-    long *longPtr = &value1;
-    value2 = *longPtr;
+    /**
+     asking the user to enter the list size
+     */
+    cout << "Enter the size of the list: ";
+    cin >> listSize;
     
-    cout << "Address of value1: " << &value1 << endl;
-    cout << "Address of longPtr: " << longPtr << endl;
-    cout << "Value pointed to value1 by longPtr: " << *longPtr << endl;
-    cout << "Value of value2: " << value2 << endl;
+    
+    int *ptr = new int[listSize];
+    
+//    /**
+//     assign a pointer to the array
+//     */
+//    int *ptr = arr;
+    
+    /**
+     this loop is for the user the enter the numbers in the list
+     */
+    for(int i = 0; i < listSize; i++){
+        cout << "Enter number into list: ";
+        cin >> value;
+        *(ptr + i) = value;
+    }
+    
+    display(ptr,listSize);
+    cout << endl;
+    cout <<"Max: " << max(ptr, listSize) << endl;
+    
     
     return 0;
 }
